3a73e9f56c0eb9da59230b981cacf643b17646f8
navyas211@gmail.com <navyas211@gmail.com>
2021-10-21 00:06:36 +0530

    [ADDED]: should have at least 1 numeric number in the password

--- src/main/java/com/bridgelabz/ur/program/UserRegistration.java
+++ src/main/java/com/bridgelabz/ur/program/UserRegistration.java
@@ -1,6 +1,6 @@
 /**
  * User need to follow pre-defined password rules.
- *Rules2- Should have at least 1 upper case.
+ *Rules3- Should have at least 1 numeric number in the password .
  * 
  * @author: Navya Shree
  * @since: 20.10.2021
@@ -47,7 +47,7 @@ public class UserRegistration {
 	// validate given phone number
 	public static void phoneNumber()  {
 		String phone = "91 9919819801";
-		String regex = "[0-9]{2}[\s][0-9]{10}";
+		String regex = "[0-9]{2}[%s][0-9]{10}";
 		boolean result = phone.matches(regex);
 		if(result)  {
 			System.out.println("Given phone number is valid");
@@ -60,7 +60,7 @@ public class UserRegistration {
 	// validate given password
 	public static void password() {
 		String password = "aBcd#123";
-		String regex = "(?=.*[A-Z])[A-Za-z0-9@_#$%!]{8,}";
+		String regex = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8,}";
 		boolean result = password.matches(regex);
 		if (result) {
 			System.out.println("Given password is valid");
