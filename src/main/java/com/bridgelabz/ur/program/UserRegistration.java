51a11c3c259c2842a19899d59b9d784817336065
navyas211@gmail.com <navyas211@gmail.com>
2021-10-21 00:02:25 +0530

    [ADDED]: follow pre-defined password rules. min 8 char

--- src/main/java/com/bridgelabz/ur/program/UserRegistration.java
+++ src/main/java/com/bridgelabz/ur/program/UserRegistration.java
@@ -1,7 +1,6 @@
 /**
-  * User need to follow pre-defiend Mobile Format .
- * Ex:-91 9919819801
- * country code follow by space and 10 digit number .
+* User need to follow pre-defined password rules.
+ * Rules1- minimum 8 characters
  * 
  * @author: Navya Shree
  * @since: 20.10.2021
@@ -19,6 +18,7 @@ public class UserRegistration {
 
 		email();
 		phoneNumber();
+		password();
 	}
 
 	// validate first name
@@ -46,13 +46,25 @@ public class UserRegistration {
 
 	// validate given phone number
 	public static void phoneNumber() {
-		String phone = "91  9919819801";
+		String phone = "91 9919819801";
 		String regex = "[0-9]{2}[%s][0-9]{10}";
 		boolean result = phone.matches(regex);
 		if (result) {
 			System.out.println("Given phone number is valid");
 		} else {
 			System.out.println("Given phone number is not valid ");
+		}
+	}
+
+	// validate given password
+	public static void password() {
+		String password = "abcd#123";
+		String regex = "[A-Za-z0-9@_#$%!]{8,}";
+		boolean result = password.matches(regex);
+		if (result) {
+			System.out.println("Given password is valid");
+		} else {
+			System.out.println("Given password is not valid");
 		}
 	}
 }
