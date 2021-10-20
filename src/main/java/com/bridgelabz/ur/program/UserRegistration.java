18abb160457105de9df9a2cabc6cafe2ec462f4e
navyas211@gmail.com <navyas211@gmail.com>
2021-10-20 23:56:24 +0530

    [ADDED]: enter a valid email

--- src/main/java/com/bridgelabz/ur/program/UserRegistration.java
+++ src/main/java/com/bridgelabz/ur/program/UserRegistration.java
@@ -14,16 +14,31 @@ public class UserRegistration {
 		System.out.println(firstName("Anokhi"));
 		System.out.println(lastName("Shree"));
 		System.out.println(lastName("Sha"));
+
+		email();
 	}
 
 	// validate first name
 	public static boolean firstName(String firstName) {
+
 		return firstName.matches("^[A-Z]{1}[a-z]{2,}$");
 	}
 
 	// validate last name
 	public static boolean lastName(String lastName) {
-		return lastName.matches("^[A-Z]{1}[a-z]{2,}$");
+		return lastName.matches("[A-z]{1}[a-z]{2,}$");
 	}
 
+	// validate given email
+	public static void email() {
+		String email = "abc.xyz@bl.co.in";
+		String regex = "^[a-z]{3}[a-zA-Z0-9+_.-]*@[a-z]{2}[.]{1}[a-z]{2}[.][a-z]*$";
+		boolean result = email.matches(regex);
+		if (result) {
+			System.out.println("Given email-id is valid");
+		} else {
+			System.out.println("Given email-id is not valid");
+		}
+
+	}
 }
